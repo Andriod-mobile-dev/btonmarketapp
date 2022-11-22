bbb00074a4fb57f124bc826c37a15382bdb3ff7e
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-21 23:00:20 -0500

    moved mainscreen to presentation module

--- app/src/main/java/com/example/btonmarket/MainActivity.kt
+++ app/src/main/java/com/example/btonmarket/MainActivity.kt
@@ -3,14 +3,9 @@ package com.example.btonmarket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.btonmarket.feature_btonmarket.presentation.home.HomeScreen
+import com.example.btonmarket.feature_btonmarket.presentation.MainScreen
 import com.example.btonmarket.ui.theme.BtonmarketTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/MainScreen.kt
+++ app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/MainScreen.kt
@@ -1,4 +1,4 @@
-package com.example.btonmarket
+package com.example.btonmarket.feature_btonmarket.presentation
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.*
