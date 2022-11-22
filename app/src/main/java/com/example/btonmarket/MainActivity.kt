ab5dc20b1a4b8ebfe59effbb60bfda094ace24ef
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-21 21:05:37 -0500

    basic navigation added

--- app/src/main/java/com/example/btonmarket/MainActivity.kt
+++ app/src/main/java/com/example/btonmarket/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.btonmarket.feature_btonmarket.presentation
+package com.example.btonmarket
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.btonmarket.feature_btonmarket.presentation.home.HomeScreen
 import com.example.btonmarket.ui.theme.BtonmarketTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BtonmarketTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BtonmarketTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/bottomNavBar/components/BottomNavGraph.kt
@@ -0,0 +1,30 @@
+package com.example.btonmarket.feature_btonmarket.presentation.bottomNavBar.components
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.btonmarket.feature_btonmarket.presentation.bottomNavBar.BottomBarScreen
+import com.example.btonmarket.feature_btonmarket.presentation.home.HomeScreen
+import com.example.btonmarket.feature_btonmarket.presentation.home.ProfileScreen
+import com.example.btonmarket.feature_btonmarket.presentation.home.SellScreen
+
+@Composable
+fun BottomNavGraph(navController: NavHostController){
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.HomeScreen.route
+    ){
+        composable(route = BottomBarScreen.HomeScreen.route){
+            HomeScreen()
+        }
+
+        composable(route = BottomBarScreen.SellScreen.route){
+            SellScreen()
+        }
+
+        composable(route = BottomBarScreen.ProfileScreen.route){
+            ProfileScreen()
+        }
+    }
+}
