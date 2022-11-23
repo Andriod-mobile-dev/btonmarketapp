c59039c6a21c1cf77b250049996d54534745e99e
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-22 20:28:52 -0500

    commit

--- app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/categories/Categories.kt
+++ app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/categories/Categories.kt
@@ -1,33 +1,41 @@
 package com.example.btonmarket.feature_btonmarket.presentation.categories
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.btonmarket.ui.theme.Purple200
 
 
 @Composable
 fun CategoriesScreen(){
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Yellow),
-        contentAlignment = Alignment.Center
-    ){
-        Text(
-            text = "Categories",
-            fontSize = MaterialTheme.typography.h3.fontSize,
-            fontWeight = FontWeight.Bold,
-            color = Color.Red
-        )
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CategoryText(category = "Home Equipment")
     }
+}
+
+
+@Composable
+fun CategoryText(category: String){
+    val GradientColors = listOf(Cyan, Color.Blue, Color.Yellow)
+
+    Text(category, fontWeight = FontWeight.Bold, color=Color.Blue)
+
+
 }
 
 @Composable
