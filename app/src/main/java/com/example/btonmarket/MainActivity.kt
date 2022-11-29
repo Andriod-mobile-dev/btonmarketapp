8ba806c6c3a5047bd4018fc222a34c7e0f1ddc2f
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-29 18:17:42 -0500

    successfully loading todo items from fake api
    Need to fetch from the actual api

--- app/src/main/java/com/example/btonmarket/MainActivity.kt
+++ app/src/main/java/com/example/btonmarket/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.example.btonmarket
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.btonmarket.feature_btonmarket.domain.model.TodoViewModel
 import com.example.btonmarket.feature_btonmarket.presentation.MainScreen
 import com.example.btonmarket.ui.theme.BtonmarketTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val vm = TodoViewModel()
         super.onCreate(savedInstanceState)
         setContent {
             BtonmarketTheme {
-                MainScreen()
+                TodoView(vm)
+                //MainScreen()
             }
         }
     }
+}
+
+@Composable
+fun TodoView(vm: TodoViewModel){
+    LaunchedEffect(key1 = Unit, block = {
+        vm.getTodoList()
+    })
+
+    Log.d("Todo items: ", vm.todoList.size.toString())
 }
 
 
