f66ac47399ba4cf53884e76fea1d2b27e540e8ca
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-29 18:38:42 -0500

    connected api result to card view

--- app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/buy/BuyScreen.kt
+++ app/src/main/java/com/example/btonmarket/feature_btonmarket/presentation/buy/BuyScreen.kt
@@ -2,7 +2,11 @@ package com.example.btonmarket.feature_btonmarket.presentation.buy
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -21,23 +25,46 @@ fun BuyScreen(onSellVm: TodoViewModel){
 
     Log.d("Buy Items: ", onSellVm.todoList.size.toString())
 
+    if(onSellVm.errorMessage.isEmpty()){
+        LazyColumn(modifier = Modifier.fillMaxSize()){
+            items(onSellVm.todoList){ onSellItems ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    BuyCardView(
+                        itemName = onSellItems.name,
+                        price = onSellItems.price,
+                        sold = onSellItems.sold,
+                        seller = onSellItems.seller,
+                        negotiable = onSellItems.negotiable,
+                        condition = onSellItems.condition
+                    )
+                }
+            }
+        }
+    }else{
+        Text(text = onSellVm.errorMessage)
+    }
+
 
     // Lazy column goes here
     // make sure to put the card at the center of the screen horizontally
-    Column(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        BuyCardView(
-            itemName = "Some",
-            price = 3000f,
-            sold = true,
-            seller = "Tesfatsion",
-            negotiable = false,
-            condition = "Very good"
-        )
-    }
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth(),
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        BuyCardView(
+//            itemName = "Some",
+//            price = 3000f,
+//            sold = true,
+//            seller = "Tesfatsion",
+//            negotiable = false,
+//            condition = "Very good"
+//        )
+//    }
 
 }
 
